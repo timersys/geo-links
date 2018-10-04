d1c0f30e5d492ee5dd5d8ce28b9ae677c76a4c8f
Damian Logghe <damian@timersys.com>
2018-10-04 14:25:12 -0300

    reformat code

--- includes/functions.php
+++ includes/functions.php
@@ -4,40 +4,43 @@
  * Grab geol settings
  * @return mixed|void
  */
-function geol_settings(){
-	$defaults = apply_filters( 'geol/settings_page/defaults', array(
-								'ajax_mode'	=> '0',
-								'goto_page'	=> 'goto',
-								'goto_url'	=> site_url('goto')
-							)
-						);
+function geol_settings() {
+	$defaults = apply_filters( 'geol/settings_page/defaults', [
+			'ajax_mode' => '0',
+			'goto_page' => 'goto',
+			'goto_url'  => site_url( 'goto' ),
+		]
+	);
 
-	$opts = wp_parse_args( get_option( 'geol_settings' ),  $defaults );
-	
-	return apply_filters('geol/settings_page/opts', $opts );
+	$opts = wp_parse_args( get_option( 'geol_settings' ), $defaults );
+
+	return apply_filters( 'geol/settings_page/opts', $opts );
 }
 
 
 /**
  * Return the redirection options
+ *
  * @param  int $id geol_cpt id
+ *
  * @return array metadata values
  */
 function geol_options( $id ) {
-	$defaults = apply_filters( 'geol/metaboxes/defaults', array(
-						'source_slug'	=> '',
-						'dest'			=> array( 'dest_0' =>
-												array(
-													'url'		=> '',
-													'ref'		=> '',
-													'country'	=> '',
-													'state'		=> '',
-													'city' 		=> '',
-													'device'	=> ''
-												)
-											)
-						)
-					);
+	$defaults = apply_filters( 'geol/metaboxes/defaults', [
+			'source_slug' => '',
+			'dest'        => [
+				'dest_0' =>
+					[
+						'url'     => '',
+						'ref'     => '',
+						'country' => '',
+						'state'   => '',
+						'city'    => '',
+						'device'  => '',
+					],
+			],
+		]
+	);
 
 	$opts = wp_parse_args( get_post_meta( $id, 'geol_options', true ), $defaults );
 
@@ -49,9 +52,9 @@ function geol_options( $id ) {
  * @return array metadata values
  */
 function geol_devices() {
-	return apply_filters('geol/devices/get_default', array(
-			'mobiles'	=> __( "Mobile Phone", 'geol' ),
-			'tablets'	=> __( "Tablet", 'geol' ),
-			'desktop'	=> __( "Dekstop", 'geol' )
-		));
+	return apply_filters( 'geol/devices/get_default', [
+		'mobiles' => __( "Mobile Phone", 'geol' ),
+		'tablets' => __( "Tablet", 'geol' ),
+		'desktop' => __( "Dekstop", 'geol' ),
+	] );
 }
